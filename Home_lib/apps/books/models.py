bf4cf7b63d33b3aa256fa521f31b5973954437de
Dmitry Morozov <9984398@gmail.com>
2020-02-06 17:38:26 +0100

    Model of books Added Model of the book instance added

--- Home_lib/apps/books/models.py
+++ Home_lib/apps/books/models.py
@@ -1,5 +1,8 @@
+import uuid
+
 from django.db import models
-from django.urls import reverse #Used to generate URLs by reversing the URL patterns
+from django.urls import reverse  # Used to generate URLs by reversing the URL patterns
+
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
@@ -17,6 +20,30 @@ class Book(models.Model):
         Returns the url to access a particular book instance.
         """
         return reverse('book-detail', args=[str(self.id)])
+
+
+class BookInstance(models.Model):
+    id = models.ForeignKey(primary_key=True, default=uuid.uuid4,
+                           help_text="Unique ID for this particular book across whole library")
+    book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
+    imprint = models.CharField(max_length=200)
+    due_back = models.DateField(null=True, blank=True)
+
+    LOAN_STATUS = (
+        ('d', 'Download'),
+        ('m', 'Maintenance'),
+        ('o', 'On loan'),
+        ('a', 'Available'),
+        ('r', 'Reserved'),
+    )
+
+    status = models.CharField(max_length=1, choices=LOAN_STATUS, blank=True, default='m', help_text='Book availability')
+
+    class Meta:
+        ordering = ['due_back']
+
+    def __str__(self):
+        return '{} ({})'.format(self.id, self.book.title)
 
 
 class Comment(models.Model):
